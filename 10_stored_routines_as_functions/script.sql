cb3353940b794843b59609920f81fe9873dbde75
TimWachira <166481796+TimWachira@users.noreply.github.com>
2024-11-21 11:15:58 +0300

    Stored Routines as Functions
    Signed-off-by: TimWachira <166481796+TimWachira@users.noreply.github.com>

--- 10_stored_routines_as_functions/script.sql
+++ 10_stored_routines_as_functions/script.sql
@@ -1,1 +1,8 @@
 -- Write your SQL code here
+CREATE FUNCTION GetExpiringMedicinesReport(cutoffDate DATE)
+RETURNS TABLE
+AS
+RETURN
+  SELECT MedicineID, MedicineName, MedicineType, ExpiryDate
+  FROM Product
+  WHERE ExpiryDate <= cutoffDate;
